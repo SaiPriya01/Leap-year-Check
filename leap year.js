f80f861cf693e941f902b171cd38634a5549fce2
SaiPriya01 <101970704+SaiPriya01@users.noreply.github.com>
2022-04-03 13:31:44 +0530

    final commit

---
+++ leap year.js
@@ -0,0 +1,11 @@
+var readlineSync=require("readline-sync");
+var userName=readlineSync.question("Please enter your name ");
+var DOB=readlineSync.question("Enter your Date Of Birth in DD-MM-YYYY format ");
+var year=DOB.split("-")[2];
+
+if ((year%400==0) || ((year%4==0)&&(year%100!=0))){
+  console.log("Hey "+userName.toUpperCase()+" !! you are born in leap year "+year);
+}
+else{
+  console.log("Hey "+userName+" !! you are not born in leap year "+year);
+}
